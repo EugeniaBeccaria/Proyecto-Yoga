a472267757651af9933d2baf6132f2a73a757cc0
Betiana-Sebben <betianasebben4@gmail.com>
2025-10-17 16:12:27 -0300

    Conflicto resuelto App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import Footer from './components/Footer.tsx';
 import Navbar from './components/Navbar.tsx';
 import HomePage from './pages/HomePage.tsx';
 import LoginPage from './pages/LoginPage.tsx';
-import RegisterPage from './pages/RegisterPage.tsx'
+import RegisterPage from './pages/RegisterPage.tsx';
 import CreateClassPage from './pages/admin/CreateClassPage.tsx';
 import { UpdateClassPage } from './pages/admin/UpdateClassPage.tsx';
 import { DeleteClassPage } from './pages/admin/DeleteClassPage.tsx';
@@ -14,114 +14,85 @@ import AdminRoutes from './components/AdminRoutes.tsx';
 import MyClassesPage from './pages/MyClassesPage.tsx';
 import ProfessorRoutes from './components/professorRoutes.tsx';
 import ProfessorDashboardPage from './pages/professor/professorDashboardPage.tsx';
-import ClasesPage from "./pages/user/ClasesPage.tsx";
-import TalleresPage from "./pages/user/TalleresPage.tsx";
-
+import ClasesPage from './pages/user/ClasesPage.tsx';
+import TalleresPage from './pages/user/TalleresPage.tsx';
 
 function App() {
-<<<<<<< HEAD
-  const hasFetched = useRef(false); // ← Para evitar doble fetch debido al strict mode 
-  const [user ,setUser] = useState<User>({  
-    id: 0,
-    email:'',
-    role:''})
-
-  async function loadUser(){
-    const userSerializado = localStorage.getItem('user')
-    if (userSerializado){
-      const userSave = JSON.parse(userSerializado)
-      setUser({
-        id:userSave.id,
-        email:userSave.email,
-        role:userSave.role
-      });
-    }
-  }
-  
-  useEffect(()=>{
-    loadUser()
-  },[])
-
-  useEffect(()=>{
-    if (hasFetched.current) return;
-    hasFetched.current = true;
-    },[user])
-  
-
-  let isAdmin = false
-  let isProfessor = false
-  let isClient = false
-  if(user.role === 'admin'){
-    isAdmin = true
-  }
-  if(user.role === "professor"){
-    isProfessor = true
-  }
-  if(user.role === "client"){
-    isClient = true
-  }
-
-=======
->>>>>>> 6f8d58f25c21392376d38767491a44492fcfd24d
-
   return (
     <>
       <Navbar />
       <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/LoginPage" element={<LoginPage />} />
-          <Route path="/RegisterPage" element={<RegisterPage />}/>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/LoginPage" element={<LoginPage />} />
+        <Route path="/RegisterPage" element={<RegisterPage />} />
         <Route path="/clases" element={<ClasesPage />} />
         <Route path="/talleres" element={<TalleresPage />} />
-          <Route path='/MyClassesPage' element={
-              <PrivateRoutes>
-                <MyClassesPage />
-              </PrivateRoutes>
-              } />
+        <Route
+          path="/MyClassesPage"
+          element={
+            <PrivateRoutes>
+              <MyClassesPage />
+            </PrivateRoutes>
+          }
+        />
 
-          <Route path="/CreateClassPage" 
+        <Route
+          path="/CreateClassPage"
           element={
             <AdminRoutes>
               <CreateClassPage />
             </AdminRoutes>
-            }/>
+          }
+        />
 
-          <Route path="/UpdateClassPage" 
+        <Route
+          path="/UpdateClassPage"
           element={
             <AdminRoutes>
               <UpdateClassPage />
             </AdminRoutes>
-            }/>
+          }
+        />
 
-          <Route path="/DeleteClassPage" 
+        <Route
+          path="/DeleteClassPage"
           element={
             <AdminRoutes>
               <DeleteClassPage />
             </AdminRoutes>
-            }/>          
-          
-          <Route path="/CreateTallerPage" element={
+          }
+        />
+
+        <Route
+          path="/CreateTallerPage"
+          element={
             <AdminRoutes>
               <CreateTallerPage />
             </AdminRoutes>
-            } />
+          }
+        />
 
-          <Route path="/MembershipPage" element={
+        <Route
+          path="/MembershipPage"
+          element={
             <AdminRoutes>
               <MembershipPage />
             </AdminRoutes>
-            } />
+          }
+        />
 
-          <Route path="/professor/dashboard" element={
+        <Route
+          path="/professor/dashboard"
+          element={
             <ProfessorRoutes>
               <ProfessorDashboardPage />
             </ProfessorRoutes>
-            } />
-
+          }
+        />
       </Routes>
-      <Footer/>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
