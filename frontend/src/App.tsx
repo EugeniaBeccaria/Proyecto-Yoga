aac8638a88e46c5f3f45fc3c90b789a669fd4da2
Betiana-Sebben <betianasebben4@gmail.com>
2025-11-06 10:40:56 -0300

    página eliminación de clases y ajustes de diseño en otras páginas

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -72,11 +72,8 @@ function App() {
                 />
 
               <Route
-<<<<<<< HEAD
-                path="/MyMembershipPage"
-=======
+                //path="/MyMembershipPage"
                 path="/MembershipPage"
->>>>>>> 9c5396d1041a1937a41387787006ccbcf133ba04
                 element={
                   <AdminRoutes>
                     <MembershipPage />
