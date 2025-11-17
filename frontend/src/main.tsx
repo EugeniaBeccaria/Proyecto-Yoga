15b8c0669e09adb98c23fbec46fcbc2f5843cd68
MechiGiaco <mechigiacolla@gmail.com>
2025-11-17 14:57:12 -0300

    merge d vuelta no se xq

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,12 +4,9 @@ import App from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import { GoogleOAuthProvider } from '@react-oauth/google';
-<<<<<<< HEAD
 import ScrollToTop from './components/ScrollToTop';
-=======
 import { loadStripe } from '@stripe/stripe-js';
 import { Elements } from '@stripe/react-stripe-js';
->>>>>>> c26bdae95f356f9d83551c5c6fb24b0bd3b4d046
 
 /*
 ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -25,19 +22,13 @@ const GOOGLE_CLIENT_ID = "961050182079-4g53a5gj6nongdd867t2medgqq1mcqun.apps.goo
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
-<<<<<<< HEAD
-      <BrowserRouter>
-        <ScrollToTop />
-          <App />
-        <ScrollToTop />
-      </BrowserRouter>
-=======
       <Elements stripe={stripePromise}>
         <BrowserRouter>
-          <App />
+          <ScrollToTop />
+            <App />
+          <ScrollToTop />
         </BrowserRouter>
       </Elements>
->>>>>>> c26bdae95f356f9d83551c5c6fb24b0bd3b4d046
     </GoogleOAuthProvider>
   </React.StrictMode>,
 );
